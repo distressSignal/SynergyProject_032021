2d8213f8970f816e198b47b409bb3a05cf93658e
laziz757 <laziz7465234@gmail.com>
2021-04-07 11:04:11 +0300

    Домашнее задание. Попытка 2. Не могу понять, как по другому реализовать метод sqrt.

--- src/java/ru/synergyitacademy/HomeWork3/HomeWork3.java
+++ src/java/ru/synergyitacademy/HomeWork3/HomeWork3.java
@@ -10,90 +10,60 @@ public class HomeWork3 {
         primeNumber();
         sqrt(49);
         System.out.println(" Введите число последовательности Фибоначчи ");
-        fib(scanner.nextInt());
-
-
+        System.out.println(fib(scanner.nextInt()));
     }
 
-    public static void fib(int n) {
-
-        int previousNumber = 0;
-        int nextNumber = 1;
-
-        for (int k = 1; k <= n; k++) {
-            int sum = previousNumber + nextNumber;
-            previousNumber = nextNumber;
-            nextNumber = sum;
-            if (n == k) {
-                System.out.println(sum);
-            }
-
-
-        }
+    public static int fib(int n) {
+        return n == 1 || n == 2 ? 1 : fib(n - 1) + fib(n - 2);
     }
 
     public static void primeNumber() {
-        boolean isprime;
         int i = 2;
         while (i <= 1000) {
-            isprime = true;
+            boolean isprime = false;
             for (int l = 2; l <= i / l; l++)
                 if (i % l == 0)
-                    isprime = false;
-            if (isprime)
+                    isprime = true;
+            if (!isprime)
                 System.out.println(i);
             i++;
         }
-
-
     }
 
-    public static void sqrt(int number) {
-        int ch2 = number;
+    public static void sqrt(int num) {
+        if (sqrtTwo(num) * sqrtTwo(num) == num) {
+            System.out.println(sqrtTwo(num));
+        } else {
+            System.out.println("Вычислить корень указанного числа невозможно");
+        }
+    }
+
+    static int sqrtTwo(int number) {
         int sc = 0;
         for (int k = 1; k <= number; k += 2) {
             sc++;
             number += -k;
         }
-        if ((sc * sc) != ch2) {
-            System.out.println("вычислить корень указанного числа невозможно");
-        } else {
-            System.out.println(sc);
-        }
-
-
+        return sc;
     }
 
     public static void quadraticEquation(double a, double b, double c) {
         if (a == 0) {
             System.out.println("Первый коэффициент не может быть 0");
-
-
         }
         double numOne;
         double numTwo;
         double numThree;
-        while (true) {
-
-
-            double d = b * b - 4 * a * c;
-            if (d > 0) {
-                numOne = (-b - Math.sqrt(d)) / (2 * a);
-                numTwo = (-b + Math.sqrt(d)) / (2 * a);
-                System.out.println(numOne + " " + numTwo);
-                break;
-
-            } else if (d == 0) {
-                numThree = -b / (2 * a);
-                System.out.println(numThree);
-                break;
-
-            } else {
-                System.out.println("Нет действительных решений уравнения");
-                break;
-            }
-
-
+        double d = b * b - 4 * a * c;
+        if (d > 0) {
+            numOne = (-b - Math.sqrt(d)) / (2 * a);
+            numTwo = (-b + Math.sqrt(d)) / (2 * a);
+            System.out.println(numOne + " " + numTwo);
+        } else if (d == 0) {
+            numThree = -b / (2 * a);
+            System.out.println(numThree);
+        } else {
+            System.out.println("Нет действительных решений уравнения");
         }
     }
 }
