5a38a3ee51bff68501007f5c4780c6dd1eab98bb
laziz757 <laziz7465234@gmail.com>
2021-04-24 10:34:55 +0300

    Домашнее задание. Вторая попытка

--- src/java/ru/synergyitacademy/HomeWork6/Animals.java
+++ src/java/ru/synergyitacademy/HomeWork6/Animals.java
@@ -1,54 +1,40 @@
 package ru.synergyitacademy.HomeWork6;
 
 public class Animals {
-    public String name = " ";
-    public Animals(String name) {
-        this.name = name;
-        voice();
-    }
 
     public Animals() {
     }
 
     public void voice() {
-        System.out.println("");
     }
 }
 
 class Dog extends Animals {
+
+    public Dog() {
+    }
+
     @Override
     public void voice() {
-        System.out.println("Гав");
+   System.out.println("Гав");;
     }
-
-    public Dog(String name) {
-        this.name = name;
-        voice();
-    }
-
 }
 
-class Cat extends Animals {
+    class Cat extends Animals {
     @Override
     public void voice() {
         System.out.println("Мяу");
     }
 
-    public Cat(String name) {
-        this.name = name;
-        voice();
+    public Cat() {
     }
 
 }
 
 class Bird extends Animals {
     public void voice() {
-        System.out.println("Чик-Чирик");
+        System.out.println("Чик-чирик");
     }
-
-    public Bird(String name) {
-        this.name = name;
-        voice();
+    public Bird() {
     }
-
 }
--- src/java/ru/synergyitacademy/HomeWork6/ArrAcc.java
+++ src/java/ru/synergyitacademy/HomeWork6/ArrAcc.java
@@ -1,10 +1,15 @@
 package ru.synergyitacademy.HomeWork6;
 
-public class ArrAcc extends Animals {
+public class ArrAcc {
     public static void main(String[] args) {
         Animals[] animals = new Animals[3];
-        animals[0] = new Cat("Марфа");
-        animals[1] = new Dog("Фэйри");
-        animals[2] = new Bird("Тефтеля");
+
+        animals[0] = new Dog();
+        animals[1] = new Cat();
+        animals[2] = new Bird();
+
+        for (Animals animal : animals) {
+            animal.voice();
+        }
     }
 }
